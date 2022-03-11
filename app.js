8fd6b23bfeb4075282ebc739b54e3f16f0a3837a
Diego Enrique Hernández Cholotío <dhernandez-2020394@kinal.edu.gt>
2022-03-11 17:22:51 -0600

    Dependencias
    adicion de las dependencias la package.json, cambios en app y en index

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// IMPORTACION RUTAS
+const productosRoutes = require('./src/routes/productos.routes');
+const ejemplosRoutes = require('./src/routes/ejemplos.routes');
+const usuarioRoutes = require('./src/routes/usuario.routes');
+const encuestasRoutes = require('./src/routes/encuestas.routes');
+
+// MIDDLEWARES
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+
+// CABECERAS
+app.use(cors());
+
+// CARGA DE RUTAS localhost:3000/api/productos
+app.use('/api', productosRoutes, ejemplosRoutes, usuarioRoutes, encuestasRoutes);
+
+module.exports = app;
