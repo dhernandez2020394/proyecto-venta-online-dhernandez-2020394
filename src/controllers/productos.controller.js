e4e7e1a98b0be5922818d50fad407788c3bc6ddb
Diego Enrique Hernández Cholotío <dhernandez-2020394@kinal.edu.gt>
2022-03-13 12:10:49 -0600

    Funnction: ObtenerProducto y EditarStockProducto
    se crearon las funciones para ediar unicamente la cantidad del producto y para obtener un unico producto

--- src/controllers/productos.controller.js
+++ src/controllers/productos.controller.js
@@ -1,43 +1,76 @@
 const Productos = require('../models/productos.model.js');
 
 //  OBTENER TODAS LOS PRODUCTOS
-function ObtenerProductos (req, res) {
+function ObtenerProductos(req, res) {
     Productos.find({}, (err, todosLosProductos) => {
 
         return res.status(200).send({ productos: todosLosProductos })
     })
 }
 
+function ObtenerProducto(req, res) {
+    var idProd = req.params.idProducto;
+
+    Productos.findById(idProd, (err, productoEcontrado) => {
+        if (err) return res.status(500).send({ mensaje: 'Error en la peticion' });
+        if (!productoEcontrado) return res.status(404)
+            .send({ mensaje: 'Error al Editar el Producto' });
+
+        return res.status(200).send({ producto: productoEcontrado });
+    })
+}
+
 // AGREGAR UN PRODUCTO
-function AgregarProducto (req, res) {
+function AgregarProducto(req, res) {
     var parametros = req.body;
     var modProductos = new Productos();
-    
-    if(parametros.nombre && parametros.cantidad && parametros.precio){
-        modProductos.nombre = parametros.nombre;
-        modProductos.cantidad = parametros.cantidad;
-        modProductos.precio = parametros.precio;
 
-        modProductos.save((err, productoGuardado)=>{
+    Productos.find({ nombre: parametros.nombre}, (err, usuarioEncontrados) => {
 
-            return res.status(200).send({ producto: productoGuardado});
-        });
-    } else {
-        return res.status(500).send({ mensaje: "Debe ingresar todos los datos obligatorios"})
-    }
+        if (usuarioEncontrados.length > 0) {
+            return res.status(500)
+            .send({ mensaje: "Ya existe un producto con el nombre " + parametros.nombre })
+        } else {
+
+            if (parametros.nombre && parametros.cantidad && parametros.precio) {
+                modProductos.nombre = parametros.nombre;
+                modProductos.cantidad = parametros.cantidad;
+                modProductos.precio = parametros.precio;
+
+                modProductos.save((err, productoGuardado) => {
+
+                    return res.status(200).send({ producto: productoGuardado });
+                });
+            } else {
+                return res.status(500).send({ mensaje: "Debe ingresar todos los datos obligatorios" })
+            }
+        }
+    })
 }
 
 // EDITAR UN PRODUCTO
 function EditarProducto(req, res) {
     var idProd = req.params.idProducto;
-    var parametros = req.body;
-
-    Productos.findByIdAndUpdate(idProd, parametros, { new : true } ,(err, productoEditado)=>{
+    var parametros = req.body
+    
+    Productos.findByIdAndUpdate(idProd, parametros, { new: true }, (err, productoEditado) => {
         if (err) return res.status(500).send({ mensaje: 'Error en la peticion' });
-        if(!productoEditado) return res.status(404)
+        if (!productoEditado) return res.status(404)
             .send({ mensaje: 'Error al Editar el Producto' });
 
-        return res.status(200).send({ producto: productoEditado});
+        return res.status(200).send({ producto: productoEditado });
+    })
+}
+
+function EditarStockProducto(req, res) {
+    var parametros = req.body;
+
+    Productos.findOneAndUpdate({nombre: parametros.nombre}, parametros, { new: true }, (err, productoEditado) => {
+        if (err) return res.status(500).send({ mensaje: 'Error en la peticion' });
+        if (!productoEditado) return res.status(404)
+            .send({ mensaje: 'Error al Editar el Stock del  Producto' });
+
+        return res.status(200).send({producto: productoEditado});
     })
 }
 
@@ -45,9 +78,9 @@ function EditarProducto(req, res) {
 function EliminarProducto(req, res) {
     var idProd = req.params.idProducto;
 
-    Productos.findByIdAndDelete(idProd, (err, productoEliminado)=>{
-        if(err) return res.status(500).send({ mensaje: 'Error en la peticion' });
-        if(!productoEliminado) return res.status(500)
+    Productos.findByIdAndDelete(idProd, (err, productoEliminado) => {
+        if (err) return res.status(500).send({ mensaje: 'Error en la peticion' });
+        if (!productoEliminado) return res.status(500)
             .send({ mensaje: 'Error al Eliminar el Producto' })
 
         return res.status(200).send({ producto: productoEliminado });
@@ -58,5 +91,7 @@ module.exports = {
     ObtenerProductos,
     AgregarProducto,
     EditarProducto,
-    EliminarProducto
+    EliminarProducto,
+    ObtenerProducto,
+    EditarStockProducto
 }
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -10,4 +10,8 @@ api.post('/agregarProducto', productosController.AgregarProducto);
 api.put('/editarProducto/:idProducto', productosController.EditarProducto);
 api.delete('/eliminarProducto/:idProducto', productosController.EliminarProducto);
 
+// FUNCIONES EXTRA
+api.get('/producto/:idProducto', productosController.ObtenerProducto);
+api.put('/editarStockProducto', productosController.EditarStockProducto);
+
 module.exports = api;
